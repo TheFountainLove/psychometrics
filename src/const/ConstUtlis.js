05753663fefb9f949833b6ec7f52a16434fb5c1d
TheFountainLove <glaiq520@163.com>
2024-02-06 17:10:44 +0800

    Revert "feat(跨域)：增加vercel跨域"
    This reverts commit 65673cbde5f9804444fb1d0fee77d8160a687fd3.

--- src/const/ConstUtlis.js
+++ src/const/ConstUtlis.js
@@ -1,8 +1,4 @@
-import {Base64} from 'js-base64'
-
-const tokenStart = 'WjJsMGFIVmlYM0JoZEY4eE1VRklRVU5LUkZrd2J6UktOMnQ0WTAweVJrODVYM0pJVVZSb1'
-const tokenEnd = 'UweE1kMWRXV0RWRE1HaDBVVFpOVEhKRFUyUkdVRUZKVEVSRE5UUklaM0UyZDJwMGIyUlhWRWxEVjFZMlNWQmtUMEZYYkdsUA=='
-const token = Base64.decode(tokenStart + tokenEnd)
+const token = 'Z2l0aHViX3BhdF8xMUFIQUNKRFkwRUtOQ0d2YlgwNjRHX3l0aFB2bnVXQ1BqTVFyZEdpRG0wVEdUcXp2OHBuallhTUE2NHlQMHlkZXdPUFhLQ0VVT2JQa1lwVnE1'
 const owner = 'TheFountainLove'
 const repo = 'psychological-assessment-data'
 const pathPrefix = 'data/psychological-assessment-data'
